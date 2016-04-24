5db47710abfec1ca9be183b04297eef16595d34c
Mike Oleynik <oleynik.mike@yandex.ru>
2016-04-24 22:03:09 +0300

    добавил задачу

---
+++ lesson2/alphabet.rb
@@ -0,0 +1,9 @@
+letters = ("a".."z")  
+vowels = ["a", "e", "i", "o", "u", "y"] 
+hash = {}
+
+letters.each_with_index do |value, id| 
+	hash[value] = id + 1 if vowels.include?(value)
+end
+
+puts hash
