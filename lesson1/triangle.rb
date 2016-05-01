bf46bf3eae3d2abebbf0c5fcd075b7bb8370f742
Mike Oleynik <oleynik.mike@yandex.ru>
2016-05-01 19:45:37 +0300

    triangle

--- lesson1/triangle.rb
+++ lesson1/triangle.rb
@@ -5,6 +5,6 @@ base = gets.chomp.to_f
 puts "Покажи мне высоту"
 height = gets.chomp.to_f
 
-square = 1/2.to_f*base*height # Можно сделать через 0.5, но мне показалось это подгон под ответ
+square = 1/2.to_f*base*height
 
 puts "Площадь треугольника равна #{square}"
