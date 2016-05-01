883bae1f580df5f13efbfb01973d48a83be51b0c
Mike Oleynik <oleynik.mike@yandex.ru>
2016-05-01 19:51:40 +0300

    equation

--- lesson1/equation.rb
+++ lesson1/equation.rb
@@ -9,7 +9,7 @@ c = gets.chomp.to_f
 
 d = b**2-4*a*c
 
-if d>0 
+if d > 0 
   y = Math.sqrt(d)
   x1 = (-b+y)/(2*a)
   x2 = (-b-y)/(2*a)
@@ -18,10 +18,10 @@ end
 
 if d == 0 
   c = 0 
-  x1=x2= -b/(2*a)
+  x1 = x2 = -b/(2*a)
   puts "#{d} и #{x1}"
 end
 
-if d<0
+if d < 0
   puts "#{d} и корней нет"
 end
