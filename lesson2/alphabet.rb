a223b00ee6cd5d4243049b4e8cdc25b9b5f39e8d
Mike Oleynik <oleynik.mike@yandex.ru>
2016-05-02 09:01:02 +0300

    refactoring :)

--- lesson2/alphabet.rb
+++ lesson2/alphabet.rb
@@ -3,7 +3,7 @@ vowels = ["a", "e", "i", "o", "u", "y"]
 hash = {}
 
 letters.each_with_index do |value, id| 
-	hash[value] = id + 1 if vowels.include?(value)
+  hash[value] = id + 1 if vowels.include?(value)
 end
 
 puts hash
